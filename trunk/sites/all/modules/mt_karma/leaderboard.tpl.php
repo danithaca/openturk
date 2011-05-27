5e1e46eba1b8ccb243339effd888bd4cc65482ad
danithaca@gmail.com <danithaca@gmail.com@f43e1abf-060f-7637-d589-20859ee9c0a7>
2011-05-27 21:29:58 +0000

    updated enset and mt_karma

--- trunk/sites/all/modules/mt_karma/leaderboard.tpl.php
+++ trunk/sites/all/modules/mt_karma/leaderboard.tpl.php
@@ -11,12 +11,17 @@
     <?php
       setlocale(LC_MONETARY, 'en_US');
       foreach ($rows as &$row) {
-        $row['data'] = $row['name'] .': '. $row['points']; // for theme_item_list()
+        //$row['data'] = "<strong>{$row['name']}</strong>: {$row['points']}"; // for theme_item_list()
+        $row['data'] = "<span class='name'>{$row['name']}</span>: <span class='points'>{$row['points']}</span>"; // for theme_item_list()
         if ($row['bonus'] > 0) {
           $bonus_str = money_format('%!.2n', $row['bonus']);
-          $row['data'] .= " (\$$bonus_str)";
-          $row['style'] = 'color:red;';
+          $row['data'] .= " <span class='bonus'>(\$$bonus_str)</span>";
+          $row['style'] = 'font-weight:bold;';
         }
+        // remove extra attributes
+        unset($row['name']);
+        unset($row['points']);
+        unset($row['bonus']);
       }
       print theme('item_list', array('items' => $rows, 'type' => 'ol'));
     ?>
