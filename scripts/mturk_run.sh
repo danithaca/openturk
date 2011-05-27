8d6ef91b83c81befb795586e260a6cb7dbad0575
danithaca@gmail.com <danithaca@gmail.com@f43e1abf-060f-7637-d589-20859ee9c0a7>
2011-05-27 21:17:23 +0000

    mt_karma update

--- scripts/mturk_run.sh
+++ scripts/mturk_run.sh
@@ -1,9 +1,10 @@
-ASYNC_COMMAND_HOME=../sites/all/modules/async_command
+OPENTURK_HOME=/home/mrzhou/openturk
+ASYNC_COMMAND_HOME=$OPENTURK_HOME/sites/all/modules/async_command
 MTURK_SDK_HOME=/opt/java-aws-mturk-1.2.2
 JYTHON_HOME=/opt/jython-2.5.2rc4
-CONFIG_FILE=../sites/default/config.properties
+CONFIG_FILE=$OPENTURK_HOME/sites/default/config.properties
 
 export CLASSPATH="${CLASSPATH}:$(find "$ASYNC_COMMAND_HOME/lib" -name "*.jar" | tr '\n' ':')"
 export CLASSPATH="${CLASSPATH}:$(find "$MTURK_SDK_HOME/lib" -name "*.jar" | tr '\n' ':')"
 
-$JYTHON_HOME/jython ../sites/all/modules/mt_task/script.py -c $CONFIG_FILE
+$JYTHON_HOME/jython $OPENTURK_HOME/sites/all/modules/mt_task/script.py -c $CONFIG_FILE
