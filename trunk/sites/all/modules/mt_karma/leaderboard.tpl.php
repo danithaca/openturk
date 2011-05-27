1a1938b60fa032e58675fdd020525f9950db5703
danithaca@gmail.com <danithaca@gmail.com@f43e1abf-060f-7637-d589-20859ee9c0a7>
2011-05-27 21:17:23 +0000

    mt_karma update

--- trunk/sites/all/modules/mt_karma/leaderboard.tpl.php
+++ trunk/sites/all/modules/mt_karma/leaderboard.tpl.php
@@ -9,8 +9,14 @@
     <p><?php print $empty; ?></p>
   <?php else: ?>
     <?php
+      setlocale(LC_MONETARY, 'en_US');
       foreach ($rows as &$row) {
         $row['data'] = $row['name'] .': '. $row['points']; // for theme_item_list()
+        if ($row['bonus'] > 0) {
+          $bonus_str = money_format('%!.2n', $row['bonus']);
+          $row['data'] .= " (\$$bonus_str)";
+          $row['style'] = 'color:red;';
+        }
       }
       print theme('item_list', array('items' => $rows, 'type' => 'ol'));
     ?>
