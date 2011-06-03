eef471ae2af84f282defe4e7415ceeb53295d54d
danithaca@gmail.com <danithaca@gmail.com@f43e1abf-060f-7637-d589-20859ee9c0a7>
2011-06-03 12:23:35 +0000

    added ctools, some other changes

--- scripts/mturk_run.sh
+++ scripts/mturk_run.sh
@@ -1,10 +1,15 @@
 OPENTURK_HOME=/home/mrzhou/openturk
 ASYNC_COMMAND_HOME=$OPENTURK_HOME/sites/all/modules/async_command
+MT_ANALYSIS_HOME=$OPENTURK_HOME/sites/all/modules/mt_analysis
+MT_TASK_HOME=$OPENTURK_HOME/sites/all/modules/mt_task
 MTURK_SDK_HOME=/opt/java-aws-mturk-1.2.2
 JYTHON_HOME=/opt/jython-2.5.2rc4
 CONFIG_FILE=$OPENTURK_HOME/sites/default/config.properties
 
+
 export CLASSPATH="${CLASSPATH}:$(find "$ASYNC_COMMAND_HOME/lib" -name "*.jar" | tr '\n' ':')"
 export CLASSPATH="${CLASSPATH}:$(find "$MTURK_SDK_HOME/lib" -name "*.jar" | tr '\n' ':')"
+export PYTHONPATH=$PATHONPATH:$MT_ANALYSIS_HOME
 
-$JYTHON_HOME/jython $OPENTURK_HOME/sites/all/modules/mt_task/script.py -c $CONFIG_FILE
+$JYTHON_HOME/jython $MT_TASK_HOME/script.py -c $CONFIG_FILE
+$JYTHON_HOME/jython $MT_ANALYSIS_HOME/script.py -c $CONFIG_FILE
