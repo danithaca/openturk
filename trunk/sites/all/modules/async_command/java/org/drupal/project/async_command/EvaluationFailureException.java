7ef14c2b296c8d1f26b0c96230a83e150654c926
danithaca@gmail.com <danithaca@gmail.com@f43e1abf-060f-7637-d589-20859ee9c0a7>
2011-11-14 00:55:11 +0000

    remove async_command

--- trunk/sites/all/modules/async_command/java/org/drupal/project/async_command/EvaluationFailureException.java
+++
@@ -1,7 +0,0 @@
-package org.drupal.project.async_command;
-
-public class EvaluationFailureException extends Exception {
-    public EvaluationFailureException(Throwable e) {
-        super(e);
-    }
-}
