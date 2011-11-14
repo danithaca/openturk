1fb4049b8f5e5c09f1e954eaf5d322f98cc4b2eb
danithaca@gmail.com <danithaca@gmail.com@f43e1abf-060f-7637-d589-20859ee9c0a7>
2011-11-14 16:28:09 +0000

    updated mturk_run.sh

--- scripts/mturk_run.sh
+++ scripts/mturk_run.sh
@@ -6,11 +6,14 @@ JYTHON_HOME=/opt/jython-2.5.2
 CONFIG_FILE=$OPENTURK_HOME/sites/default/config.properties
 
 
-export CLASSPATH="${CLASSPATH}:$(find "$ASYNC_COMMAND_HOME/lib" -name "*.jar" | tr '\n' ':')"
+export CLASSPATH="$(find "$ASYNC_COMMAND_HOME/lib" -name "*.jar" | tr '\n' ':')"
 export CLASSPATH="${CLASSPATH}:$(find "$MTURK_SDK_HOME/lib" -name "*.jar" | tr '\n' ':')"
-export PYTHONPATH=$PYTHONPATH:$MTURK_MODULE_HOME/mt_task:$MTURK_MODULE_HOME/mt_analysis
+export CLASSPATH=$ASYNC_COMMAND_HOME/drupal-app.jar:$CLASSPATH
+#export PYTHONPATH=$PYTHONPATH:$MTURK_MODULE_HOME/mt_task:$MTURK_MODULE_HOME/mt_analysis
+PYTHONPATH=$PYTHONPATH:$MTURK_MODULE_HOME/mt_task:$MTURK_MODULE_HOME/mt_analysis:$MTURK_MODULE_HOME/mt_bonus:$MTURK_MODULE_HOME/mt_karma
 export JYTHONPATH=$PYTHONPATH
 
-$JYTHON_HOME/jython $MTURK_MODULE_HOME/mt_task/mt_task.py -c $CONFIG_FILE
-$JYTHON_HOME/jython $MTURK_MODULE_HOME/mt_analysis/mt_analysis.py -c $CONFIG_FILE
-$JYTHON_HOME/jython $MTURK_MODULE_HOME/mt_bonus/mt_bonus.py -c $CONFIG_FILE
+#$JYTHON_HOME/jython $MTURK_MODULE_HOME/mt_task/mt_task.py -c $CONFIG_FILE
+#$JYTHON_HOME/jython $MTURK_MODULE_HOME/mt_analysis/mt_analysis.py -c $CONFIG_FILE
+#$JYTHON_HOME/jython $MTURK_MODULE_HOME/mt_bonus/mt_bonus.py -c $CONFIG_FILE
+$JYTHON_HOME/jython -Dpython.security.respectJavaAccessibility=false $MTURK_MODULE_HOME/mturk.py
